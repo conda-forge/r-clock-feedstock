917eb317c96cb7361ac7fef3ecd7677010a6e2b8
Mervin Fansler <mmfansler@gmail.com>
2023-06-14 19:10:10 -0400

    ignore osx sdk warnings

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,5 +2,9 @@
 
 export DISABLE_AUTOBREW=1
 
+# workaround for OSX 10.9 targetting
+# https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk
+CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
+
 # shellcheck disable=SC2086
 ${R} CMD INSTALL --build . ${R_ARGS}
