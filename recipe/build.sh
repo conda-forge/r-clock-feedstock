871a5a879405ec4f5b082c05322307e2890b0819
Mervin Fansler <mmfansler@gmail.com>
2023-06-14 23:15:28 -0400

    cpp not cxx

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,7 +4,7 @@ export DISABLE_AUTOBREW=1
 
 # workaround for OSX 10.9 targetting
 # https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk
-export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
+export CPPFLAGS="${CPPFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
 
 # shellcheck disable=SC2086
 ${R} CMD INSTALL --build . ${R_ARGS}
