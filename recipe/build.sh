81bb3a32bd6770f1a1e9ba619c7437b73f6d15c3
Mervin Fansler <mmfansler@gmail.com>
2023-06-14 23:46:07 -0400

    revert

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,9 +2,5 @@
 
 export DISABLE_AUTOBREW=1
 
-# workaround for OSX 10.9 targetting
-# https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk
-export CPPFLAGS="${CPPFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
-
 # shellcheck disable=SC2086
 ${R} CMD INSTALL --build . ${R_ARGS}
