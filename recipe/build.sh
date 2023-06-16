587fd979aa703d9e7388c195dc3228a696bbb192
Mervin Fansler <mmfansler@gmail.com>
2023-06-16 02:27:40 -0400

    user makevars

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+mkdir -p ~/.R
+echo "CXXFLAGS = -D_LIBCPP_DISABLE_AVAILABILITY" >> ~/.R/Makevars
+
 export DISABLE_AUTOBREW=1
 
 # shellcheck disable=SC2086
 ${R} CMD INSTALL --build . ${R_ARGS}
+
+rm -rf ~/.R
